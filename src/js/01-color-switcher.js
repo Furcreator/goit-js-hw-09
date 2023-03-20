af21b022a4a896f1f22db716495358624a5e3857
Daniil Mikhnevich <dan.mikhnevich20@gmail.com>
2023-03-20 18:33:55 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+
+  const startBtn = document.querySelector("button[data-start]");
+  const stopBtn = document.querySelector("button[data-stop]");
+
+  startBtn.addEventListener("click", onClickBtnStart);
+  stopBtn.addEventListener("click", onClickBtnStop);
+
+
+  function onClickBtnStart(e) {
+    e.preventDefault();
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    timeColorId = setInterval(()=>{
+        const color = getRandomHexColor();
+        document.body.style.backgroundColor = color;
+    }, 1000)
+
+  }
+  function onClickBtnStop(e) {
+    e.preventDefault();
+    clearInterval(timeColorId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const inputForm = document.querySelector('.form');
+inputForm.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e){
+    const stepInp = inputForm.step.value;
+    const delayInp = inputForm.delay.value;
+    const amountInp = inputForm.amount.value;
+e.preventDefault();
+let delayTime = Number(delayInp);
+console.log(delayTime);
+for (let i = 1; i <= amountInp; i++) {
+    createPromise(i, delayTime).then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+      delayTime += Number(stepInp)
+      // console.log(delayTime);
+}}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (shouldResolve) {
+            resolve({position, delay});
+
+        } else {
+          reject({position, delay});
+        }
+    }, delay);
+  });
 }
