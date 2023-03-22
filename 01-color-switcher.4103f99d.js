1aba9b1f9d51b2ec4799c37aec9831c553f0f3e7
Furcreator <114292729+Furcreator@users.noreply.github.com>
2023-03-22 08:58:47 +0000

    Deploying to gh-pages from @ Furcreator/goit-js-hw-09@4a5583373b4294755d6fdeeae52d49b6a026b313 🚀

---
+++ 01-color-switcher.4103f99d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");function a(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.backgroundColor=t}t.addEventListener("click",(function(n){n.preventDefault(),t.disabled=!0,e.disabled=!1,timeId=setInterval(a,1e3)})),e.addEventListener("click",(function(a){a.preventDefault(),clearInterval(timeId),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.4103f99d.js.map
--- 01-color-switcher.d84609e4.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(a){a.preventDefault(),t.disabled=!0,e.disabled=!1,timeId=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(a){a.preventDefault(),clearInterval(timeId),t.disabled=!1,e.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.d84609e4.js.map
