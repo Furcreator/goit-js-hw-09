4a5583373b4294755d6fdeeae52d49b6a026b313
Daniil Mikhnevich <dan.mikhnevich20@gmail.com>
2023-03-22 10:58:19 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,28 +1,34 @@
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
 
-  const startBtn = document.querySelector("button[data-start]");
-  const stopBtn = document.querySelector("button[data-stop]");
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
 
-  startBtn.addEventListener("click", onClickBtnStart);
-  stopBtn.addEventListener("click", onClickBtnStop);
+startBtn.addEventListener("click", onClickBtnStart);
+stopBtn.addEventListener("click", onClickBtnStop);
 
 
-  function onClickBtnStart(e) {
-    e.preventDefault();
-    startBtn.disabled = true;
-    stopBtn.disabled = false;
-    timeId = setInterval(()=>{
-        const color = getRandomHexColor();
-        document.body.style.backgroundColor = color;
-    }, 1000)
+function updateClock() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+}
 
-  }
-  function onClickBtnStop(e) {
-    e.preventDefault();
-    clearInterval(timeId);
-    startBtn.disabled = false;
-    stopBtn.disabled = true;
-  }
+function onClickBtnStart(e) {
+  e.preventDefault();
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timeId = setInterval(updateClock, 1000);
+  // timeId = setInterval(()=>{
+  //     const color = getRandomHexColor();
+  //     document.body.style.backgroundColor = color;
+  // }, 1000)
+
+}
+function onClickBtnStop(e) {
+  e.preventDefault();
+  clearInterval(timeId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
